9dc3fe3d8ac0e9590a9b194d46ea7e8c100dc998
Brandon <bwwebs@gmail.com>
2020-06-29 18:02:53 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Auto updating of changes is love!
         </p>
         <a
           className="App-link"
